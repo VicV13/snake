821fb671208d37f52630f1b004cd19129adc06fe
inna <dbrdbrd@gmail.com>
2016-03-27 18:01:34 +0300

    Создали класс Point

--- brains/brains/Program.cs
+++ brains/brains/Program.cs
@@ -14,20 +14,14 @@ namespace brains
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
-            /*
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
 
-            Draw(x1, y1, sym1);*/
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            p2.Draw();
 
             Console.ReadLine();
         }
